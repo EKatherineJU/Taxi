18972d2e6c077229b997a19672c141cfcc224bfb
EKatherineJU <elianajaramillo74842@correo.itm.edu.co>
2020-03-25 21:35:08 -0500

    Correction change and recovery password MVC

--- Taxi.Web/Controllers/AccountController.cs
+++ Taxi.Web/Controllers/AccountController.cs
@@ -38,13 +38,13 @@ namespace Taxi.Web.Controllers
             _mailHelper = mailhelper;
         }
 
-        public IActionResult RecoverPassword()
+        public IActionResult RecoverPasswordMVC()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> RecoverPassword(RecoverPasswordViewModel model)
+        public async Task<IActionResult> RecoverPasswordMVC(RecoverPasswordViewModel model)
         {
             if (ModelState.IsValid)
             {
@@ -137,13 +137,13 @@ namespace Taxi.Web.Controllers
             return BadRequest();
         }
 
-        public IActionResult ChangePassword()
+        public IActionResult ChangePasswordMVC()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        public async Task<IActionResult> ChangePasswordMVC(ChangePasswordViewModel model)
         {
             if (ModelState.IsValid)
             {
